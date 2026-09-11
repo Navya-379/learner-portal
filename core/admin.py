ab49b4f650a0f51b7e86b24ded69201ce81db06f
Navya Sree <navya221204@gmail.com>
2026-09-11 23:13:34 +0530

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -21,7 +21,7 @@ class ProgressInline(admin.TabularInline):
 # CustomUser admin
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    inlines = [EnrollmentInline, QuizResultInline, ProgressInline]
+    
     list_display = ("username", "email", "role", "is_active", "is_staff", "date_joined")
     search_fields = ("username", "email")
     ordering = ("username",)
