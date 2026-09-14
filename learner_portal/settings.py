93147a9272c0059c02a46813a191c4016af1813f
Navya Sree <navya221204@gmail.com>
2026-09-14 18:43:27 +0530

    Update settings.py

--- learner_portal/settings.py
+++ learner_portal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',  # 👈 your app
+    'core.apps.CoreConfig',  # 👈 your app
 ]
 AUTH_USER_MODEL = "core.CustomUser"
 
