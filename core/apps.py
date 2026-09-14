07bc7829e93be8a77a5fed8603880fe06b614da3
Navya Sree <navya221204@gmail.com>
2026-09-14 18:41:37 +0530

    Update apps.py
    Connect post_migrate signal to create a superuser if it doesn't exist.

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+import os
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        def create_superuser(sender, **kwargs):
+            User = get_user_model()
+            username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+            email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+            password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+            if username and password and not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+
+        post_migrate.connect(create_superuser, sender=self)
