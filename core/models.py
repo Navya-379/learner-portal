8e3880d96ba4e3b5ec853a7683382c7372b8959a
Navya Sree <navya221204@gmail.com>
2026-09-23 20:09:04 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -106,4 +106,6 @@ class Progress(models.Model):
         return (self.completed_units / self.total_units) * 100
 
     def __str__(self):
-        return f"{self.enrollment.user.username} - {self.enrollment.course.title} ({self.completion_percentage():.2f}%)"
+    user = getattr(self.enrollment.user, "username", "Unknown User")
+    course = getattr(self.enrollment.course, "title", "Unknown Course")
+    return f"{user} - {course} ({self.completion_percentage():.2f}%)"
